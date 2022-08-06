bd27b81ea7a0ccc71d89c7207b3f44e83704fd8a
youthfulhps <ybh942002@gmail.com>
2022-08-06 20:18:20 +0900

    Feature: Apply theme switch to renderer App

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,40 +1,12 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
-import './App.css';
+import ThemeSwitch from '@ThemeSwitch/components';
+import '@shared/styles/index.scss';
 
-const Hello = () => {
+const Main = () => {
   return (
     <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
+      <h1>테스트입니다</h1>
+      <ThemeSwitch />
     </div>
   );
 };
@@ -43,7 +15,7 @@ export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
   );
