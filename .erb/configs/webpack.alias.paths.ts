ff4c40abbbd8f81f0c14f65ab90a66248980cedf
youthfulhps <ybh942002@gmail.com>
2022-08-14 16:47:20 +0900

    Setting: Add user profile alias paths to webpaack config

--- .erb/configs/webpack.alias.paths.ts
+++ .erb/configs/webpack.alias.paths.ts
@@ -3,6 +3,7 @@ const path = require('path');
 const aliasPaths = {
   '@shared': path.resolve(__dirname, '../../src/renderer/shared'),
   '@ThemeSwitch': path.resolve(__dirname, '../../src/renderer/ThemeSwitch'),
+  '@UserProfile': path.resolve(__dirname, '../../src/renderer/UserProfile'),
 };
 
 export default aliasPaths;
