762899d2bf892330ab38a513368ebb9e9e6b23a7
youthfulhps <ybh942002@gmail.com>
2022-08-18 09:57:09 +0900

    Setting: Add alias paths to webpack config

--- .erb/configs/webpack.alias.paths.ts
+++ .erb/configs/webpack.alias.paths.ts
@@ -4,6 +4,7 @@ const aliasPaths = {
   '@shared': path.resolve(__dirname, '../../src/renderer/shared'),
   '@ThemeSwitch': path.resolve(__dirname, '../../src/renderer/ThemeSwitch'),
   '@UserProfile': path.resolve(__dirname, '../../src/renderer/UserProfile'),
+  '@Registration': path.resolve(__dirname, '../../src/renderer/Registration'),
 };
 
 export default aliasPaths;
