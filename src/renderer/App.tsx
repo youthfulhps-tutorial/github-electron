d600bbff8d9cf3a7da242c6f2d1f81dd2a1432e5
youthfulhps <ybh942002@gmail.com>
2022-08-21 17:51:37 +0900

    Feature: Apply registration component to main

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,14 +2,21 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ThemeSwitch from '@ThemeSwitch/components';
+import Registration from '@Registration/components';
+import UserContextProvider, { UserContext } from '@shared/contexts/UserContext';
 import '@shared/styles/index.scss';
+import { useContext } from 'react';
 
 const Main = () => {
+  const { hasUserId } = useContext(UserContext);
+
+  if (!hasUserId) return <Registration />;
+
   return (
-    <div>
+    <>
       <h1>테스트입니다</h1>
       <ThemeSwitch />
-    </div>
+    </>
   );
 };
 
@@ -25,12 +32,14 @@ export default function App() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
-      </Router>
-      <ReactQueryDevtools />
+      <UserContextProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Main />} />
+          </Routes>
+        </Router>
+        <ReactQueryDevtools />
+      </UserContextProvider>
     </QueryClientProvider>
   );
 }
