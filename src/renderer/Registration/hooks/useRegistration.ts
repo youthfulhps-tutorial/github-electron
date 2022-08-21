77744d8b74f16cab5ceb557bab6b43b78dd1b75f
youthfulhps <ybh942002@gmail.com>
2022-08-21 17:44:42 +0900

    Feature: Implement loading state when verifying user id

--- src/renderer/Registration/hooks/useRegistration.ts
+++ src/renderer/Registration/hooks/useRegistration.ts
@@ -1,32 +1,58 @@
 import { getUser } from '@shared/apis/user';
 import { debounce } from '@shared/utils/common';
-import { KeyboardEventHandler, useCallback, useState } from 'react';
+import { useCallback, useState, useContext } from 'react';
+import { UserContext } from '@shared/contexts/UserContext';
 
 const useRegistration = () => {
   const [inputValue, setInputValue] = useState<string>('');
   const [isValidate, setIsValidate] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isFadingOut, setIsFadingOut] = useState<boolean>(false);
 
-  const checkUserValidation = useCallback(
-    debounce(async (userId: string) => {
-      try {
-        const user = await getUser(userId);
-        setIsValidate(user.data.login === userId);
-      } catch (e) {
-        setIsValidate(false);
-      }
-    }, 1500),
+  const { setUserId } = useContext(UserContext);
+
+  const checkUserValidation = useCallback(async (userId: string) => {
+    setIsLoading(true);
+    try {
+      const user = await getUser(userId);
+      setIsValidate(user.data.login === userId);
+    } catch (e) {
+      setIsValidate(false);
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  const debouncedUserValidation = useCallback(
+    debounce((userId: string) => checkUserValidation(userId), 1500),
     []
   );
 
-  const handleInputChange: KeyboardEventHandler<HTMLInputElement> = useCallback(
-    (e) => {
-      setInputValue(e.target.value);
-      checkUserValidation(e.target.value);
-    },
-    [checkUserValidation]
-  );
+  const changeInputValue = useCallback((userId: string) => {
+    setInputValue(userId);
+    debouncedUserValidation(userId);
+  }, []);
 
-  return { isValidate, inputValue, handleInputChange };
+  const successRegistration = useCallback(() => {
+    setUserId(inputValue);
+    setIsFadingOut(true);
+  }, []);
+
+  const cancelRegistration = useCallback(() => {
+    setIsValidate(false);
+    setInputValue('');
+  }, []);
+
+  return {
+    isLoading,
+    setIsLoading,
+    isValidate,
+    inputValue,
+    isFadingOut,
+    changeInputValue,
+    successRegistration,
+    cancelRegistration,
+  };
 };
 
 export default useRegistration;
