250d479b560b4bcd041c048e4eae68fe417987ee
youthfulhps <ybh942002@gmail.com>
2022-08-24 01:36:58 +0900

    Feature: Apply user profile component to App

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ThemeSwitch from '@ThemeSwitch/components';
 import Registration from '@Registration/components';
+import UserProfile from '@UserProfile/components';
 import UserContextProvider, { UserContext } from '@shared/contexts/UserContext';
 import '@shared/styles/index.scss';
 import { useContext } from 'react';
@@ -16,6 +17,7 @@ const Main = () => {
     <>
       <h1>테스트입니다</h1>
       <ThemeSwitch />
+      <UserProfile />
     </>
   );
 };
