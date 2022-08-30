681d9df26b631c706eca6849e3de02e00497ea87
youthfulhps <ybh942002@gmail.com>
2022-08-30 22:49:29 +0900

    Setting: Add environment variable for development

--- src/renderer/shared/apis/index.ts
+++ src/renderer/shared/apis/index.ts
@@ -4,6 +4,9 @@ const BASE_URL = 'https://api.github.com';
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    Authorization: `Token ${process.env.ELECTRON_GITHUB_DEVELOP_TOKEN}`,
+  },
 });
 
 export default axiosInstance;
